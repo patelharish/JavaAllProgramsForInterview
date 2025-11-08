5a71277b43d80b08b221068a63eb5734d01807e5
Harish patel <hkp@gmail.com>
2025-11-08 22:36:17 +0530

    added some more code

--- Strings/manipulation/WAP12ab34ghg56.java
+++ Strings/manipulation/WAP12ab34ghg56.java
@@ -1,9 +1,12 @@
 package Strings.manipulation;
 
+import java.util.Arrays;
+
 public class WAP12ab34ghg56 {
     public static void main(String[] args) {
         String str = "12ab34ghg,56";
         String[] sr = str.split("[^0-9]+");
+        System.out.println(Arrays.toString(sr));
         StringBuilder sb = new StringBuilder();
         int sum = 0;
         for(String s : sr){
